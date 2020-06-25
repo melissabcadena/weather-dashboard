c2838ceed5a18ccf7cf99b1bdf19a6480ff43079
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-25 18:21:01 -0500

    create function to perform upon city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,42 @@
+var searchFormEl = document.querySelector("#city-search-submit");
+var cityInputEl = document.querySelector("#city-input");
+var previousSearchesEl = document.querySelector("#previous-searches");
+
 var getWeather = function (cityName) {
+    // format api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=8fc9d3841b8ffcc0fce5fb6a16a654cc"
 
+    // make request to url
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
-            console.log(response.json());
+            response.json().then(function(data) {
+                console.log(data);
+            })
         } else {
             alert("Error: " + response.statusText);
         }
     })
 }
 
-getWeather("dallas");
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    
+    var requestedCity = cityInputEl.value.trim();
+    if (requestedCity) {
+        // add city to previously searched cities
+        var previousCityListItem = document.createElement("li");
+        previousCityListItem.className = "nav-item";
+        previousCityListItem.innerHTML="<a class='nav-link' href='#'><span data-feather='file'></span>" + requestedCity + "</a>";
+
+        previousSearchesEl.appendChild(previousCityListItem);
+        
+        // push through to get weather function
+        getWeather(requestedCity);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter valid city.");
+    }
+}
+
+searchFormEl.addEventListener("click", formSubmitHandler);
